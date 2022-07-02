478bc9eb9ed775beac4a600caa3038787ee82f34
eggr0ll <pomme@skooge.com>
2022-07-01 21:23:38 -0500

    Choropleth map works with fips codes

--- src/tn.py
+++ src/tn.py
@@ -12,24 +12,27 @@ counties['features'] = [f for f in counties['features'] if f['properties']['STAT
 # create dataframes
 df = pd.read_csv('data/Copy-of-Zip-Code-Report-(11)-clean.csv', usecols=['CTYNAME', 'attendance'], dtype={'fips': str})
 df2 = pd.read_csv('data/county-populations.csv', usecols=['CTYNAME', 'pop2022'])
+df3 = pd.read_csv('data/county_fips.csv')
 
 # merge df and df2
 final_df = pd.merge(df, df2, on='CTYNAME')
+# delete "County" from names in CTYNAME column
+final_df['CTYNAME'] = final_df['CTYNAME'].str.replace(' County', '')
+# merge to df3
+final_df = pd.merge(final_df, df3, on='CTYNAME')
 # find percentage (attendance from 1 county / total ACS attendance)
 final_df['percent-attendance-total'] = ((final_df['attendance'] / final_df['attendance'].sum()) * 100)
 # find percentage (attendance from 1 county / 1 county's population)
 final_df['percent-attendance-cty'] = ((final_df['attendance'] / final_df['pop2022']) * 100)
-# delete "County" from names in CTYNAME column
-final_df['CTYNAME'] = final_df['CTYNAME'].str.replace(' County', '')
 # rename columns
-final_df.rename(columns={'percent-attendance-total': 'percent_attendance_total', 'percent-attendance-cty': 'percent_attendance_cty', 'CTYNAME': 'NAME'}, inplace=True)
+final_df.rename(columns={'percent-attendance-total': 'percent_attendance_total', 'percent-attendance-cty': 'percent_attendance_cty'}, inplace=True)
 
 # create map
-fig = px.choropleth(data_frame=final_df, locations='NAME', geojson=counties, color='percent_attendance_total',
+fig = px.choropleth(data_frame=final_df, locations='fips', geojson=counties, color='percent_attendance_total',
                     color_continuous_scale='Viridis',
                     range_color=(0, 12),
                     scope='usa',
                     )
-#fig.update_geos(fitbounds="locations", visible=True)
+fig.update_geos(fitbounds="locations", visible=False)
 fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
 fig.show()
