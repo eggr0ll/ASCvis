7a381ae3be7b62819d4dcbb99ada427461748fc9
eggr0ll <pomme@skooge.com>
2022-07-01 19:49:16 -0500

    Updated Readme, merged dataframes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ASCvis
+Initially, I am going to make 2 maps:
+- One that displays a county's attendance out of the total ASC attendance (as a percent).
+- One that displays a county's attendance out of the same county's populations (as a percent).
+
+Later, data from the other ASC locations will be included.
+
+For the county coordinates & other info, I used the `.json` file from plotly's choropleth article.
--- src/tn.py
+++ src/tn.py
@@ -1,43 +1,35 @@
-from urllib.request import urlopen
-import json
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
-
-import pandas as pd
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
-                   dtype={"fips": str})
-
-import plotly.express as px
-
-fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 12),
-                           scope="usa",
-                           labels={'unemp':'unemployment rate'}
-                          )
-
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
-
-
 import plotly.express as px
 import requests
 import json
 import pandas as pd
+
 
 r = requests.get('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json')
 counties = json.loads(r.text)
 target_states = ['47']
 counties['features'] = [f for f in counties['features'] if f['properties']['STATE'] in target_states]
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv', dtype={'fips': str})
+# create dataframes
+df = pd.read_csv('data/Copy-of-Zip-Code-Report-(11)-clean.csv', usecols=['CTYNAME', 'attendance'], dtype={'fips': str})
+df2 = pd.read_csv('data/county-populations.csv', usecols=['CTYNAME', 'pop2022'])
 
-fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
+# merge df and df2
+final_df = pd.merge(df, df2, on='CTYNAME')
+# find percentage (attendance from 1 county / total ACS attendance)
+final_df['percent-attendance-total'] = ((final_df['attendance'] / final_df['attendance'].sum()) * 100)
+# find percentage (attendance from 1 county / 1 county's population)
+final_df['percent-attendance-cty'] = ((final_df['attendance'] / final_df['pop2022']) * 100)
+# delete "County" from names in CTYNAME column
+final_df['CTYNAME'] = final_df['CTYNAME'].str.replace(' County', '')
+# rename columns
+final_df.rename(columns={'percent-attendance-total': 'percent_attendance_total', 'percent-attendance-cty': 'percent_attendance_cty', 'CTYNAME': 'NAME'}, inplace=True)
+
+# create map
+fig = px.choropleth(data_frame=final_df, locations='NAME', geojson=counties, color='percent_attendance_total',
                     color_continuous_scale='Viridis',
                     range_color=(0, 12),
                     scope='usa',
-                    labels={'unemp': 'unemployment rate'}
                     )
-fig.update_geos(fitbounds="locations", visible=False)
+#fig.update_geos(fitbounds="locations", visible=True)
 fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
 fig.show()
